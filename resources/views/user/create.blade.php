d10b5cb1996332924d7ed8fd2d47529a20d5885f
Tino <tinosirka@gmail.com>
2019-07-03 09:03:40 +0200

    Style changes added to create page

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -3,58 +3,68 @@
 
 @section('content')
 
-    <!-- Main content -->
-    {!! Form::open(['route'=>'store.user', 'method'=>'POST', 'class'=>'validate']) !!}
-<section class="content">
-          <div class="row">
-              <div class="col-md-6 members-position">
+<!-- Main content -->
+{!! Form::open(['route'=>'store.user', 'method'=>'POST', 'class'=>'validate']) !!}
 
-              @include('partials.errors')
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Create User</h1>
+    </div>
 
-                <div class="box">
-                      <div class="box-body">
-                <div class="form-group">
-                  <label>First Name</label>
-                  {!! Form::input('text', 'first_name', null, ['class'=>'form-control']) !!}
+    <div class="row">
+        <div class="col-md-6 members-position">
+
+                @include('partials.errors')
+
+                <div class="card mb-4">
+
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label>First Name</label>
+                            {!! Form::input('text', 'first_name', null, ['class'=>'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            <label>Last Name</label>
+                            {!! Form::input('text', 'last_name', null, ['class'=>'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            <label>Email</label>
+                            {!! Form::input('text', 'email', null, ['class'=>'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            <label>Password</label>
+                            {!! Form::input('password', 'password', null, ['class'=>'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            <label>Repeat Password</label>
+                            {!! Form::input('password', 'password_confirmation', null, ['class'=>'form-control']) !!}
+                        </div>
+                    </div>
+
+                     <div class="create-user">
+
+                        <div class="back">
+                            <a href="{{route('index.users')}}" class="btn btn-primary d-none d-sm-inline-block">Back</a>
+                        </div>
+
+                        <div class="save-user">
+                            <button class="btn btn-success d-none d-sm-inline-block">Save</button>
+                        </div>
+
+                    </div>
+
                 </div>
 
-               <div class="form-group">
-               <label>Last Name</label>
-                  {!! Form::input('text', 'last_name', null, ['class'=>'form-control']) !!}
-               </div>
-
-               <div class="form-group">
-               <label>Email</label>
-                  {!! Form::input('text', 'email', null, ['class'=>'form-control']) !!}
-               </div>
-
-                <label>Password</label>
-                  {!! Form::input('password', 'password', null, ['class'=>'form-control']) !!}
-               </div>
-
-               <label>Repeat Password</label>
-                  {!! Form::input('password', 'password_confirmation', null, ['class'=>'form-control']) !!}
-               </div>
-
-
-
-
-    <div class="box-footer">
-         <div class="pull-left">
-         <a href="{{route('index.users')}}" class="btn btn-primary btn-lg">Back</a>
-         </div>
-
-         <div class="pull-right">
-             <button class="btn btn-success btn-lg">Save</button>
-         </div>
-
-                      </div>
-                 </div>
-             </div>
-
+            </div>
         </div>
-</section>
-    {!! Form::close() !!}
+    </div>
 
-    <!-- /.content -->
+    </div>
+</section>
+{!! Form::close() !!}
+
+<!-- /.content -->
 @endsection
