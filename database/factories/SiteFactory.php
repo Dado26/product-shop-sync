6ef30b8bc5c2071abb2013defc57819c2bd0563f
Vladimir Struhar <vstruhar@gmail.com>
2019-07-03 12:34:23 +0200

    small site factory change

--- database/factories/SiteFactory.php
+++ database/factories/SiteFactory.php
@@ -6,9 +6,11 @@ use App\Models\Site;
 use Faker\Generator as Faker;
 
 $factory->define(Site::class, function (Faker $faker) {
+    $name = $faker->domainWord;
+
     return [
-        'name' => $faker->domainWord,
-        'url' => $faker->url,
+        'name' => ucfirst($name),
+        'url' => "https://www.$name.com",
         'email' => $faker->safeEmail,
     ];
 });
