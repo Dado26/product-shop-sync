42c36c8071f58ff718e383c5ecb03a6f72eb7e6b
Tino <tinosirka@gmail.com>
2019-07-04 08:33:15 +0200

    small style changes

--- resources/views/sites/edit.blade.php
+++ resources/views/sites/edit.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.main')
 
-
 @section('content')
-
-
-
 
 <!-- Main content -->
 <form action="{{ route('sites.update', $site->id) }}" method="POST" class="form-horizontal">
@@ -17,104 +13,127 @@
 
     <div class="row">
 
-        <div class="col-md-10 members-position">
+        <div class="col-md-6 members-position">
 
-        @include('partials.errors')
+            @include('partials.errors')
 
-        <div class="card shadow mb-4">
+            <div class="card shadow mb-4">
 
-            <div class="card-body">
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Name</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="sites[name]" class="form-control" value="{{$site->name}}">
-                    </div>
+                <div class="card-header">
+                    General
                 </div>
 
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">URL</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="sites[url]" class="form-control" value="{{$site->url}}">
-                    </div>
-                </div>
+                <div class="card-body">
 
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Email</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="sites[email]" class="form-control" value="{{$site->email}}">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Title</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="sync_Rules[title]" class="form-control" value="{{ $site->SyncRules->title}}">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Description</label>
-                    <div class="col-sm-12">
-                        <textarea rows="5" cols="80" name="description"class="form-control">{{ $site->SyncRules->description}}</textarea>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Price</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="name" class="form-control" value="{{ $site->SyncRules->price }}">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">In Stock</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="url" class="form-control" value="{{  $site->SyncRules->in_stock}}">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">In_stock_value</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="name" class="form-control" value="{{ $site->SyncRules->in_stock_value}}">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Images</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="url" class="form-control" value="{{ $site->SyncRules->images }}">
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Variants</label>
-                    <div class="col-sm-12">
-                        <input type="text" name="url" class="form-control" value="{{ $site->SyncRules->variants }}">
-                    </div>
-                </div>
-
-            </div>
-
-                <div class="card-footer d-flex justify-content-between">
-                    <div class="back">
-                        <a href="{{route('sites.index') }}" class="btn btn-primary d-none d-sm-inline-block">
-                            <i class="fas fa-angle-left"></i> Back
-                        </a>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Name</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="sites[name]" class="form-control" value="{{$site->name}}">
+                        </div>
                     </div>
 
-                    <div class="save-user">
-                        <button type="submit" class="btn btn-success d-none d-sm-inline-block">
-                            <i class="far fa-save"></i> Save
-                        </button>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">URL</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="sites[url]" class="form-control" value="{{$site->url}}">
+                        </div>
                     </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="sites[email]" class="form-control" value="{{$site->email}}">
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
 
         </div>
 
+        <div class="col-lg-6 members-position">
+
+            <div class="card shadow mb-4">
+
+                <div class="card-header">
+                    Scrape rules
+                </div>
+
+                <div class="card-body">
+
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Title</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="sync_Rules[title]" class="form-control" value="{{ $site->SyncRules->title}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Description</label>
+                        <div class="col-sm-12">
+                            <textarea rows="5" cols="80" name="description"class="form-control">{{ $site->SyncRules->description}}</textarea>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Price</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="name" class="form-control" value="{{ $site->SyncRules->price }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">In Stock</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="url" class="form-control" value="{{  $site->SyncRules->in_stock}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">In_stock_value</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="name" class="form-control" value="{{ $site->SyncRules->in_stock_value}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Images</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="url" class="form-control" value="{{ $site->SyncRules->images }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Variants</label>
+                        <div class="col-sm-12">
+                            <input type="text" name="url" class="form-control" value="{{ $site->SyncRules->variants }}">
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="d-flex justify-content-center mt-5 mb-4">
+        <div class="col-8 d-flex justify-content-between">
+            <div class="back">
+                <a href="{{route('sites.index') }}" class="btn btn-primary d-none d-sm-inline-block btn-lg">
+                    <i class="fas fa-angle-left"></i> Back
+                </a>
+            </div>
+
+            <div class="save-user">
+                <button type="submit" class="btn btn-success d-none d-sm-inline-block btn-lg">
+                    <i class="far fa-save"></i> Save
+                </button>
+            </div>
+        </div>
     </div>
 
 </form>
