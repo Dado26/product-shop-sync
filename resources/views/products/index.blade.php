7fb517206d325e5b1424d7ac3116b6e6ed6d41df
Tino <tinosirka@gmail.com>
2019-07-05 13:01:11 +0200

    Style changes on products page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -10,16 +10,21 @@
     <div class="card-body d-flex justify-content-between">
 
         <form class="d-none d-sm-inline-block form-inline navbar-search col-8">
-            <div class="input-group input-group-prepend">
-                <button class="btn btn-light border-light" type="button">url</button>
+            <div class="input-group">
+
+                <div class="input-group-append">
+                    <button class="btn btn-light border-light rounded-left" type="button">
+                        url
+                    </button>
+                </div>
                 <input type="text" class="form-control border-light" placeholder="https://sample.rs/product/123" aria-label="Search" aria-describedby="basic-addon2">
             </div>
         </form>
 
         <div class="col-4 d-flex justify-content-between">
 
-            <div class="dropdown d-inline-block">
-                <button class="btn border-light bg-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <div class="dropdown d-inline-block w-50 ml-4">
+                <button class="btn border-light bg-light dropdown-toggle w-100" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dropdown
                 </button>
                 <div class="dropdown-menu animated--fade-in" aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
@@ -44,6 +49,18 @@
 
 <div class="card shadow mt-5">
     <div class="card-body">
+
+        <form class="d-flex d-inline-block mb-4 w-50">
+            <div class="input-group">
+                <input type="text" class="form-control border-light" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-light border-light" type="button">
+                        Go!
+                    </button>
+                </div>
+            </div>
+        </form>
+
         <table class="table table-bordered">
             <tr class="card-header">
                 <th class="text-center">ID</th>
@@ -89,9 +106,6 @@
 
                     <td class="text-center">
                         <div class="users-list-actions">
-                            <button type="button" data-toggle="modal" data-target="#confirmButton" class="btn btn-primary d-none d-sm-inline-block btn-sm rounded-circle">
-                                <i class="fas fa-sync-alt"></i>
-                            </button>
                             <button type="button" data-toggle="modal" data-target="#confirmButton" class="btn btn-primary d-none d-sm-inline-block btn-sm">
                                 <i class="fas fa-sync-alt"></i> Sync
                             </button>
