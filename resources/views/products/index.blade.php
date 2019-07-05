7b44a76b741a1df05338ab81f375bb9b9393ddc7
Tino <tinosirka@gmail.com>
2019-07-05 10:37:55 +0200

    Style nav bar on products page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,27 +2,40 @@
 
 @section('content')
 
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
+<div class="d-sm-flex align-items-center mb-4">
     <h1 class="h4 mb-0 text-gray-800">Products</h1>
 </div>
 
-<div class="card mb-4">
+<div class="card">
     <div class="card-body d-flex justify-content-between">
 
-        <form class="d-none d-sm-inline-block form-inline navbar-search">
-            <div class="input-group">
-                <input type="text" class="form-control bg-light border-0" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                <div class="input-group-append">
-                    <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i> Search
-                    </button>
-                </div>
+        <form class="d-none d-sm-inline-block form-inline navbar-search col-8">
+            <div class="input-group input-group-prepend">
+                <button class="btn btn-light border-light" type="button">url</button>
+                <input type="text" class="form-control border-light" placeholder="https://sample.rs/product/123" aria-label="Search" aria-describedby="basic-addon2">
             </div>
         </form>
 
-        <a href="{{ route('sites.create') }}" class="d-none d-sm-inline-block btn btn-primary shadow-sm">
-            <i class="fas fa-plus"></i> import
-        </a>
+        <div class="col-4 d-flex justify-content-between">
+
+            <div class="dropdown d-inline-block">
+                <button class="btn border-light bg-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </button>
+                <div class="dropdown-menu animated--fade-in" aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+
+            <div class="d-inline-block">
+                <a href="{{ route('sites.create') }}" class="d-none d-sm-inline-block btn btn-primary shadow-sm">
+                    <i class="fas fa-plus"></i> import
+                </a>
+            </div>
+
+        </div>
 
     </div>
 </div>
