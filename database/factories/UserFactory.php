034507bbe15d817b84ae9dc8619c15c081878fc9
Daniel <danielklaudija@gmail.com>
2019-07-08 16:17:14 +0200

    diagram update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,7 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\User;
+use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
