213c80ae2b7606d81697bf3cce4d1b512e850c84
vstruhar <vstruhar@gmail.com>
2019-07-30 11:53:24 -0400

    updated pull.sh

--- pull.sh
+++ pull.sh
@@ -7,14 +7,18 @@ php artisan down
 # Pull the latest changes from the git repository
 # git reset --hard
 # git clean -df
+printf "Pulling changes from master branch -----------------------------------------------\n"
 git pull origin master
 
 # Install/update composer dependecies
+printf "Composer install -----------------------------------------------------------------\n"
 composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
 # Run database migrations
+printf "Running migrations----------------------------------------------------------------\n"
 php artisan migrate --force
 
+printf "Clearing caches ------------------------------------------------------------------\n"
 # Clear caches
 php artisan cache:clear
 
@@ -30,16 +34,22 @@ php artisan config:clear
 php artisan config:cache
 
 # Queue restart
+printf "Restarting queues ----------------------------------------------------------------\n"
 php artisan queue:restart
 
 # Restart Horizon
+printf "Terminating horizon --------------------------------------------------------------\n"
 php artisan horizon:terminate
 
 # Install npm dependencies
+printf "Installing npm dependencies ------------------------------------------------------\n"
 npm install
 
 # Build assets using Laravel Mix
+printf "Building production assets -------------------------------------------------------\n"
 npm run production
 
 # Turn off maintenance mode
 php artisan up
+
+printf "Deploy has finished successfully -------------------------------------------------\n"
