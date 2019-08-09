1a7452dcb41519f25f10974bca3324e497f6de89
Vladimir Struhar <vstruhar@gmail.com>
2019-08-10 00:04:40 +0200

    removed telescope from production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,11 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() !== 'production') {
+        if (app()->environment('local')) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
+            $this->app->register(TelescopeServiceProvider::class);
 
-        Telescope::ignoreMigrations();
+            Telescope::ignoreMigrations();
+        }
     }
 
     /**
