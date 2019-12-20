9e32ac0c2d17233f1898c4bad2a699ee307536b9
Vladimir Struhar <vstruhar@gmail.com>
2019-12-20 19:55:53 +0100

    update

--- app/Helpers/StringHelper.php
+++ app/Helpers/StringHelper.php
@@ -11,6 +11,7 @@ class StringHelper
      */
     public static function keepLettersAndNumbers(string $string)
     {
+        // keep only letters (even cyrillic) and numbers
         return preg_replace('/\s(\S*)\s/u', '', $string);
     }
 }
