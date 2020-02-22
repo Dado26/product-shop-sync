c476a57c229baa9b23c62b6216fda55c02f96513
Vladimir Struhar <vstruhar@gmail.com>
2020-02-22 01:37:02 +0100

    crawl images fix

--- app/Services/ProductCrawlerService.php
+++ app/Services/ProductCrawlerService.php
@@ -150,6 +150,8 @@ class ProductCrawlerService
 
         $specifications = $this->crawler->filter($rule)->html();
 
+        $specifications = strip_tags($specifications, ['p', 'tr', 'td', 'tbody', 'b', 'strong', 'i', 'th']);
+
         return '<table>' . trim($specifications) . '</table>';
     }
 
